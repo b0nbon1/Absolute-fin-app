b4a3a56176ab87739be660b963500d5d007e3a3b
Bonvic Bundi <bonvic@Bonvics-MacBook-Pro.local>
2025-02-03 10:22:45 +0300

    switch to db/sql

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -1,12 +1,11 @@
 package db
 
 import (
-	"context"
+	"database/sql"
 	"log"
 	"os"
 	"testing"
-
-	"github.com/jackc/pgx/v5/pgxpool"
+	_ "github.com/lib/pq"
 )
 
 const (
@@ -15,14 +14,16 @@ const (
 )
 
 var testQueries *Queries
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	conn, err := pgxpool.New(context.Background(), dbSource)
+	var err error
+	testDB, err =  sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
