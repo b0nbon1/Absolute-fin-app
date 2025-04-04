d99175ddfdf006c61bd3d12b84579a90baf0c787
Bonvic <bundi.bonvic@gmail.com>
2025-04-04 06:00:31 +0300

    add env files and variables

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -5,12 +5,9 @@ import (
 	"log"
 	"os"
 	"testing"
-	_ "github.com/lib/pq"
-)
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:1234567890@localhost:5432/simple_bank?sslmode=disable"
+	"github.com/b0nbon1/bank-system/util"
+	_ "github.com/lib/pq"
 )
 
 var testQueries *Queries
@@ -18,7 +15,13 @@ var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 	var err error
-	testDB, err =  sql.Open(dbDriver, dbSource)
+
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
+
+	testDB, err =  sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
--- main.go
+++ main.go
@@ -6,17 +6,17 @@ import (
 
 	"github.com/b0nbon1/bank-system/api"
 	db "github.com/b0nbon1/bank-system/db/sqlc"
+	"github.com/b0nbon1/bank-system/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:1234567890@localhost:5432/simple_bank?sslmode=disable"
-	serverAddr = ":8080"
-)
-
 func main() {
-	conn, err :=  sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
+
+	conn, err :=  sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
@@ -24,11 +24,11 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddr)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server: ", err)
 	}
 
-	log.Println("server started at", serverAddr)
+	log.Println("server started at", config.ServerAddress)
 
 }
